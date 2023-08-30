fca6e1180adafbefcb6ea268572e9e6253f443a5
hayat-khan-space <143410457+hayat-khan-space@users.noreply.github.com>
2023-08-30 12:45:40 +0500

    Update main.py

--- flatten_json/main.py
+++ flatten_json/main.py
@@ -1,37 +1,25 @@
-def _flatten_json_(self, y):
+def _flatten_json_(y):
         out = {}
         def flatten(x, name=''):
             if type(x) is dict:
-                #print "xb:", x ," size :", len(x)
                 if len(x):
                     for a in x:
-                        #print "x[a]:", x[a], " name ", name, " a ", a
                         flatten(x[a], name + a + '.')
-                    #print "xs:", x
                 elif x == {}:
-                    #print "xs_0:", x, "name: ",name
                     out[name[:-1]] = x
             elif type(x) is list:
-                #print "lb:", x
                 i = 0
-                #print "lb:", x, " size :", len(x)
                 if len(x):
-                    #print "ls_N0:", x, "name: ", name
-                    #for a in x:
-                    #    flatten(a, name + str(i) + '.')
-                    #    i += 1
                     out[name[:-1]] = x
                 elif(x==[]):
-                    #print "ls_0:", x, "name: ", name
                     out[name[:-1]] = x
             else:
-                #print "e:", x
                 out[name[:-1]] = x
 
         flatten(y)
         return out
 
-    def _unflatten_json_(self, dic):
+def _unflatten_json_(dic):
         try:
             resultDict = dict()
             for key, value in dic.items():
@@ -47,3 +35,7 @@ def _flatten_json_(self, y):
             logging.getLogger('EventLog').log(logging.INFO, "unflatten json error: %s" % (str(e.message)),
                                               extra=EventType.INDIVIDUAL_ASSET._dict)
         return dict()
+dict_string = {'type': '1', 'data': {'key1': 'k', 'key2': 'k2'}}
+flatten = _flatten_json_(dict_string)
+unflatten = _unflatten_json_(flatten)
+print "dict_string", dict_string,"","_flatten_json_",flatten," \n ","unflatten: ",unflatten
